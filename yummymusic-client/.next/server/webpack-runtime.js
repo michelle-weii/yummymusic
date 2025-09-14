cb60885a6550661a844deaa78aca896930d48b13
William Gao <williamgao@college.harvard.edu>
2025-09-14 06:01:05 -0700

    Integrated frontend with modal, finalized modal setup

--- yummymusic-client/.next/server/webpack-runtime.js
+++ yummymusic-client/.next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("438142fa5497dafc")
+/******/ 		__webpack_require__.h = () => ("1f557ec86e1fc780")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
